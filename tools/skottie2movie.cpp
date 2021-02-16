9a6452feaaa3a6e150a06d8ad5f5aad050d4760c
Mohamed Irshaam <mohamed.irshaam@gmail.com>
2021-02-17 00:08:14 +0800

    fix:  warning: identifier contains Unicode character "

--- tools/skottie2movie.cpp
+++ tools/skottie2movie.cpp
@@ -29,40 +29,40 @@ static DEFINE_bool2(verbose, v, false, "verbose mode");
 static DEFINE_bool2(loop, l, false, "loop mode for profiling");
 static DEFINE_int(set_dst_width, 0, "set destination width (height will be computed)");
 static DEFINE_bool2(gpu, g, false, "use GPU for rendering");
-​
+
 static DEFINE_int_2(red, r, 255, "Background red component");
 static DEFINE_int_2(blue, b, 255, "Background blue component");
 static DEFINE_int_2(green, g, 255, "Background green component");
 static DEFINE_int_2(alpha, a, 255, "Background alpha component");
-​
+
 static void produce_frame(SkSurface* surf, skottie::Animation* anim, double frame, SkColor color) {
     anim->seekFrame(frame);
     surf->getCanvas()->clear(color);
     anim->render(surf->getCanvas());
 }
-​
+
 struct AsyncRec {
     SkImageInfo info;
     SkVideoEncoder* encoder;
 };
-​
+
 int main(int argc, char** argv) {
     SkGraphics::Init();
-​
+
     CommandLineFlags::SetUsage("Converts skottie to a mp4");
     CommandLineFlags::Parse(argc, argv);
-​
+
     SkColor backgroundColor = SkColorSetARGB(FLAGS_alpha, FLAGS_red, FLAGS_blue, FLAGS_green);
-​
+
     if (FLAGS_input.count() == 0) {
         SkDebugf("-i input_file.json argument required\n");
         return -1;
     }
-​
+
     auto contextType = sk_gpu_test::GrContextFactory::kGL_ContextType;
     GrContextOptions grCtxOptions;
     sk_gpu_test::GrContextFactory factory(grCtxOptions);
-​
+
     SkString assetPath;
     if (FLAGS_assetPath.count() > 0) {
         assetPath.set(FLAGS_assetPath[0]);
@@ -70,7 +70,7 @@ int main(int argc, char** argv) {
         assetPath = SkOSPath::Dirname(FLAGS_input[0]);
     }
     SkDebugf("assetPath %s\n", assetPath.c_str());
-​
+
     auto animation =
             skottie::Animation::Builder()
                     .setResourceProvider(skresources::FileResourceProvider::Make(assetPath))
@@ -79,41 +79,41 @@ int main(int argc, char** argv) {
         SkDebugf("failed to load %s\n", FLAGS_input[0]);
         return -1;
     }
-​
+
     SkISize dim = animation->size().toRound();
     double duration = animation->duration();
     int fps = SkTPin(FLAGS_fps, 1, 240);
     double fps_scale = animation->fps() / fps;
-​
+
     float scale = 1;
     if (FLAGS_set_dst_width > 0) {
         scale = FLAGS_set_dst_width / (float)dim.width();
         dim = {FLAGS_set_dst_width, SkScalarRoundToInt(scale * dim.height())};
     }
-​
+
     const int frames = SkScalarRoundToInt(duration * fps);
     const double frame_duration = 1.0 / fps;
-​
+
     if (FLAGS_verbose) {
         SkDebugf("Size %dx%d duration %g, fps %d, frame_duration %g\n", dim.width(), dim.height(),
                  duration, fps, frame_duration);
     }
-​
+
     SkVideoEncoder encoder;
-​
+
     GrDirectContext* context = nullptr;
     sk_sp<SkSurface> surf;
     sk_sp<SkData> data;
-​
+
     const auto info = SkImageInfo::MakeN32Premul(dim);
     do {
         double loop_start = SkTime::GetSecs();
-​
+
         if (!encoder.beginRecording(dim, fps)) {
             SkDEBUGF("Invalid video stream configuration.\n");
             return -1;
         }
-​
+
         // lazily allocate the surfaces
         if (!surf) {
             if (FLAGS_gpu) {
@@ -133,17 +133,15 @@ int main(int argc, char** argv) {
             }
             surf->getCanvas()->scale(scale, scale);
         }
-​
+
         for (int i = 0; i <= frames; ++i) {
             const double frame = i * fps_scale;
             if (FLAGS_verbose) {
                 SkDebugf("rendering frame %g\n", frame);
             }
-​
-            // produce_frame(surf.get(), animation.get(), frame);
-​
+
             produce_frame(surf.get(), animation.get(), frame, backgroundColor);
-​
+
             AsyncRec asyncRec = {info, &encoder};
             if (context) {
                 auto read_pixels_cb = [](SkSurface::ReadPixelsContext ctx,
@@ -164,19 +162,19 @@ int main(int argc, char** argv) {
             }
         }
         data = encoder.endRecording();
-​
+
         if (FLAGS_loop) {
             double loop_dur = SkTime::GetSecs() - loop_start;
             SkDebugf("recording secs %g, frames %d, recording fps %d\n", loop_dur, frames,
                      (int)(frames / loop_dur));
         }
     } while (FLAGS_loop);
-​
+
     if (FLAGS_output.count() == 0) {
         SkDebugf("missing -o output_file.mp4 argument\n");
         return 0;
     }
-​
+
     SkFILEWStream ostream(FLAGS_output[0]);
     if (!ostream.isValid()) {
         SkDebugf("Can't create output file %s\n", FLAGS_output[0]);
